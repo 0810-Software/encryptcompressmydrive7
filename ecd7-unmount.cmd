db70c859bbe297dc1f8b3569422970698f65a85e
Marnix0810 <48680596+Marnix0810@users.noreply.github.com>
2019-10-14 00:16:25 +0200

    Add unmount

---
+++ ecd7-unmount.cmd
@@ -0,0 +1,23 @@
+REM @powershell -window hidden -command ""
+:search
+for %%p in (A B D E F G H I J K L M N O P Q R S T U V W X Y Z) do if  exist %%p:\.ecmd.8 (
+set ecmdrive7=%%p:
+goto start
+)
+if "%ecmdrive7%"=="" (
+goto search
+)
+:start
+FOR /F "usebackq tokens=1,2* delims=," %%G IN ("%ecmdrive7%\ecd7db.files\sav.ecd7db") DO SET %%G
+del /f /q "%ecmdrive7%\ecd7db.files\sav.ecd7db"
+del /f /q  "%ecmdrive7%\ecmd7.files\%Uname7%.ecmd7"
+cd /d "%ecmdlocation7%"
+md "%tmp%\ecmd7\%mountpointnumber7%"
+%~dp0bin\7za a "%ecmdrive7%\ecmd7.files\%Uname7%.ecmd7" * -t7z -r -sdel -w"%tmp%\ecmd7\%mountpointnumber7%"
+cd /d "%~dp0"
+subst /d %Mounttovolume7%
+set "Mounttovolume7="
+rd /S /Q "%ecmdlocation7%"
+ren %ecmdrive7%\.ecmd.8 .ecmd.7
+start /wait mshta "javascript:alert('You can now use Safe Eject to eject %ecmdrive7%.');window.close()"
+pause
