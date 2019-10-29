6aafcd3389a9315e5dd50eb8684d524d391bd916
Marnix0810 <48680596+Marnix0810@users.noreply.github.com>
2019-10-29 16:26:49 +0100

    Update ecd7-ft.cmd

--- ecd7-ft.cmd
+++ ecd7-ft.cmd
@@ -4,13 +4,13 @@ md "%tmp%\ecd7-formattingtool_%ft7sessionnumber%.tmp"
 cd /d "%tmp%\ecd7-formattingtool_%ft7sessionnumber%.tmp"
 set "restorewd=cd /d %tmp%\ecd7-formattingtool_%ft7sessionnumber%.tmp"
 :selectadrive
-FOR /f "delims=" %%a in ('mshta.exe "%tmp%\ecd7-formattingtool_%ft7sessionnumber%.tmp\GUI\Select.drive1.hta"') do set drive=%%a
+FOR /f "delims=" %%a in ('mshta.exe "%~dp0GUI\Select.drive1.hta"') do set drive=%%a
 if not exist "%drive%" (
 start /wait mshta "javascript:alert('This drive cannot be found.');window.close()"
 goto selectadrive
 )
 set "quickswitch="
-FOR /f "delims=" %%a in ('mshta.exe "%tmp%\ecd7-formattingtool_%ft7sessionnumber%.tmp\GUI\Quickswitchmenu1.hta"') do set quick=%%a
+FOR /f "delims=" %%a in ('mshta.exe "%~dp0GUI\Quickswitchmenu1.hta"') do set quick=%%a
 if "%quick%"=="1" set quickswitch=/q
 format %DRIVE% /x /v:ecmd7 /fs:exFAT /y %quickswitch%
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://github.com/Marnix0810/encryptcompressmydrive7/raw/master/files/ecd7-fs.7z', 'ecd7-fs_latest.7z') }"
